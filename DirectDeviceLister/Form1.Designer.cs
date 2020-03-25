fe34a24d5054c5be18f39c358307f354a2f87a80
Angelo <Soviet_Trooper@live.ca>
2020-03-24 17:12:59 -0700

    Initial commit

---
+++ DirectDeviceLister/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DirectDeviceLister
+{
+    partial class DirectDeviceListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TrVi_List = new System.Windows.Forms.TreeView();
+            this.Bu_RefreshList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TrVi_List
+            // 
+            this.TrVi_List.Location = new System.Drawing.Point(12, 12);
+            this.TrVi_List.Name = "TrVi_List";
+            this.TrVi_List.Size = new System.Drawing.Size(776, 397);
+            this.TrVi_List.TabIndex = 0;
+            // 
+            // Bu_RefreshList
+            // 
+            this.Bu_RefreshList.Location = new System.Drawing.Point(12, 415);
+            this.Bu_RefreshList.Name = "Bu_RefreshList";
+            this.Bu_RefreshList.Size = new System.Drawing.Size(776, 23);
+            this.Bu_RefreshList.TabIndex = 1;
+            this.Bu_RefreshList.Text = "Refresh List";
+            this.Bu_RefreshList.UseVisualStyleBackColor = true;
+            this.Bu_RefreshList.Click += new System.EventHandler(this.Bu_RefreshList_Click);
+            // 
+            // DirectDeviceListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Bu_RefreshList);
+            this.Controls.Add(this.TrVi_List);
+            this.Name = "DirectDeviceListForm";
+            this.Text = "Direct Device List";
+            this.Load += new System.EventHandler(this.DirectDeviceListForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TrVi_List;
+        private System.Windows.Forms.Button Bu_RefreshList;
+    }
+}
+
---
+++ DirectDeviceLister/Form1.cs
@@ -0,0 +1,98 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectDeviceLister
+{
+    public partial class DirectDeviceListForm : Form
+    {
+        static DirectInput DirectInputDriversInfo = new DirectInput();
+
+        public DirectDeviceListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DirectDeviceListForm_Load(object sender, EventArgs e)
+        {
+            ListDirectDevices();
+        }
+
+        private void Bu_RefreshList_Click(object sender, EventArgs e)
+        {
+            ListDirectDevices();
+        }
+
+        void ListDirectDevices()
+        {
+            TrVi_List.Nodes.Clear();
+
+            TrVi_List.Nodes.Add("Devices");
+            DeviceInstance[] Devices = DirectInputDriversInfo.GetDevices(DeviceType.Device, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in Devices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("ControlDevices");
+            DeviceInstance[] ControlDevices = DirectInputDriversInfo.GetDevices(DeviceType.ControlDevice, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in ControlDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("DrivingDevices");
+            DeviceInstance[] DrivingDevices = DirectInputDriversInfo.GetDevices(DeviceType.Driving, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in DrivingDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("FirstPersonDevices");
+            DeviceInstance[] FirstPersonDevices = DirectInputDriversInfo.GetDevices(DeviceType.FirstPerson, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in FirstPersonDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("FlightDevices");
+            DeviceInstance[] FlightDevices = DirectInputDriversInfo.GetDevices(DeviceType.Flight, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in FlightDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("GamePadDevices");
+            DeviceInstance[] GamePadDevices = DirectInputDriversInfo.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in GamePadDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("JoystickDevices");
+            DeviceInstance[] JoystickDevices = DirectInputDriversInfo.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in JoystickDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("KeyboardDevices");
+            DeviceInstance[] KeyboardDevices = DirectInputDriversInfo.GetDevices(DeviceType.Keyboard, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in KeyboardDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("MouseDevices");
+            DeviceInstance[] MouseDevices = DirectInputDriversInfo.GetDevices(DeviceType.Mouse, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in MouseDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("RemoteDevices");
+            DeviceInstance[] RemoteDevices = DirectInputDriversInfo.GetDevices(DeviceType.Remote, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in RemoteDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("ScreenPointerDevices");
+            DeviceInstance[] ScreenPointerDevices = DirectInputDriversInfo.GetDevices(DeviceType.ScreenPointer, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in ScreenPointerDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+
+            TrVi_List.Nodes.Add("SupplementalDevices");
+            DeviceInstance[] SupplementalDevices = DirectInputDriversInfo.GetDevices(DeviceType.Supplemental, DeviceEnumerationFlags.AttachedOnly).ToArray();
+            foreach (DeviceInstance Dev in SupplementalDevices)
+                TrVi_List.Nodes[TrVi_List.Nodes.Count - 1].Nodes.Add($"{Dev.ProductName} - {Dev.ProductGuid} - {Dev.InstanceName} - {Dev.InstanceGuid}");
+        }
+    }
+}
